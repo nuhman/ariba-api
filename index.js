74cf19d5e34b508c3a778c14f8ef12e2636ee1aa
Muhammed Nuhman <nuhman7@outlook.com>
2024-06-11 01:13:20 +0530

    fix response

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ app.post("/punchout", (req, res) => {
   console.log('Received cXML:', JSON.stringify(cxml, null, 2));
 
   const response = `<?xml version="1.0" encoding="UTF-8"?>
-    <cXML payloadID="${generatePayloadID()}" timestamp="${getCurrentTimestamp()}">
+    <!DOCTYPE cXML SYSTEM "http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd">
+    <cXML xml:lang="en-US" payloadID="${generatePayloadID()}" timestamp="${getCurrentTimestamp()}">
       <Response>
         <Status code="200" text="OK" />
         <PunchOutSetupResponse>
